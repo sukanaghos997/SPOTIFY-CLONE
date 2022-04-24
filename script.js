278d4d6aacec88e87a9ce2eaefc05682f890a369
SUKANA GHOSH <88514250+sukanaghos997@users.noreply.github.com>
2022-04-25 00:15:51 +0530

    master

--- script.js
+++ script.js
@@ -23,8 +23,14 @@ let songs = [
     {songName: "Tumhari Kasam - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/9.jpg"},
     {songName: "Na Jaana - Salam-e-Ishq", filePath: "songs/4.mp3", coverPath: "covers/10.jpg"},
 ]
-// audioElement.play(); 
 
+songItems.forEach((element, i)=>{ 
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath; 
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName; 
+})
+
+// audioElement.play(); 
+ 
 //handle play/pause click     
 masterPlay.addEventListener('click', () => {
         if(audioElement.paused || audioElement.currentTime<=0){
@@ -50,60 +56,6 @@ audioElement.addEventListener('timeupdate', () =>{   //time limit of the music
     myProgressBar.value = progress;
 })
 
-// myProgressBar.addEventListener('change', ()=>{
-//     audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
-// })
-
-// const makeAllPlays = ()=>{
-//     Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
-//         element.classList.remove('fa-pause-circle');
-//         element.classList.add('fa-play-circle');
-//     })
-// }
-
-// Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
-//     element.addEventListener('click', (e)=>{ 
-//         makeAllPlays();
-//         songIndex = parseInt(e.target.id);
-//         e.target.classList.remove('fa-play-circle');
-//         e.target.classList.add('fa-pause-circle');
-//         audioElement.src = `songs/${songIndex+1}.mp3`;
-//         masterSongName.innerText = songs[songIndex].songName;
-//         audioElement.currentTime = 0;
-//         audioElement.play();
-//         gif.style.opacity = 1;
-//         masterPlay.classList.remove('fa-play-circle');
-//         masterPlay.classList.add('fa-pause-circle');
-//     })
-// })
-
-// document.getElementById('next').addEventListener('click', ()=>{
-//     if(songIndex>=9){
-//         songIndex = 0
-//     }
-//     else{
-//         songIndex += 1;
-//     }
-//     audioElement.src = `songs/${songIndex+1}.mp3`;
-//     masterSongName.innerText = songs[songIndex].songName;
-//     audioElement.currentTime = 0;
-//     audioElement.play();
-//     masterPlay.classList.remove('fa-play-circle');
-//     masterPlay.classList.add('fa-pause-circle');
-
-// })
-
-// document.getElementById('previous').addEventListener('click', ()=>{
-//     if(songIndex<=0){
-//         songIndex = 0
-//     }
-//     else{
-//         songIndex -= 1;
-//     }
-//     audioElement.src = `songs/${songIndex+1}.mp3`;
-//     masterSongName.innerText = songs[songIndex].songName;
-//     audioElement.currentTime = 0;
-//     audioElement.play();
-//     masterPlay.classList.remove('fa-play-circle');
-//     masterPlay.classList.add('fa-pause-circle');
-// })
+    myProgressBar.addEventListener('change', ()=>{
+    audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
+})
